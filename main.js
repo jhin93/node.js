6605f306accb0fa3967089832e26a1c71d18f302
jhin93 <scene1993@gmail.com>
2021-11-09 12:19:43 +0900

    data 폴더에 입력만 해도 알아서 읽고 내용도 추가됨

--- main.js
+++ main.js
@@ -12,10 +12,25 @@ var app = http.createServer(function(request,response){
     if(pathname === '/'){
       // 하지만 pathname만으로는 메인페이지와 사이드페이지들을 구분할 수 없기에 .id로 쿼리스트링이 undefined일 때 즉, 메인페이지를 조건으로 새롭게 렌더링.
       if(queryData.id === undefined){
-        // 파일 읽어오기. data 폴더의 파일을 fs.readFile로 읽어옴. 쿼리스트링에 따라 파일명이 생성됨. utf8로 인코딩.
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+
+        // 파일 목록을 가져오는 코드로 감싸기. data 폴더의 내용물을 가져오면 콜백함수 실행.
+        fs.readdir('./data', function(error, filelist){
+          // 파일리스트 가져오기.
+          console.log(filelist);
+          // 파일리스트 가져오고 렌더링파트(var template) 삽입.
           var title = 'Welcome';
           var description = 'Hello, Node.js';
+
+          // while 문으로 리스트 자동생성. filelist 배열을 가져옴
+          var list = '<ul>'
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+            i = i + 1;
+          }
+
+          list = list + '</ul>';
+
           // 1.html을 복사해와서 백쿼테이션 사이에 삽입.
           // 2. a href를 '/?id='를 사용해서 대입. 이를 node.js의 var url = require('url');에서 query string으로 가져옴.
             var template = `
@@ -27,11 +42,7 @@ var app = http.createServer(function(request,response){
               </head>
               <body>
                 <h1><a href="/">WEB</a></h1>
-                <ul>
-                  <li><a href="/?id=HTML">HTML</a></li>
-                  <li><a href="/?id=CSS">CSS</a></li>
-                  <li><a href="/?id=Javascript">JavaScript</a></li>
-                </ul>
+                ${list}
                 <h2>${title}</h2>
                 <p>${description}</p>
               </body>
@@ -39,34 +50,44 @@ var app = http.createServer(function(request,response){
             `
             response.writeHead(200);
             response.end(template);
-        });
+        })
+
       } else {
-        // 파일 읽어오기. data 폴더의 파일을 fs.readFile로 읽어옴. 쿼리스트링에 따라 파일명이 생성됨. utf8로 인코딩.
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var title = queryData.id
-          // 1.html을 복사해와서 백쿼테이션 사이에 삽입.
-          // 2. a href를 '/?id='를 사용해서 대입. 이를 node.js의 var url = require('url');에서 query string으로 가져옴.
-          var template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              <ul>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=Javascript">JavaScript</a></li>
-              </ul>
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>      
-          `
-          response.writeHead(200);
-          response.end(template);
+        fs.readdir('./data', function(error, filelist){
+
+          // while 문으로 리스트 자동생성. filelist 배열을 가져옴
+          var list = '<ul>'
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+            i = i + 1;
+          }
+
+        list = list + '</ul>';
+          // 여기부턴 쿼리스트링에 값이 있는 사이드 페이지들.
+          // 파일 읽어오기. data 폴더의 파일을 fs.readFile로 읽어옴. 쿼리스트링에 따라 파일명이 생성됨. utf8로 인코딩.
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id
+            // 1.html을 복사해와서 백쿼테이션 사이에 삽입.
+            // 2. a href를 '/?id='를 사용해서 대입. 이를 node.js의 var url = require('url');에서 query string으로 가져옴.
+            var template = `
+              <!doctype html>
+              <html>
+              <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+              </head>
+              <body>
+                <h1><a href="/">WEB</a></h1>
+                ${list}
+                <h2>${title}</h2>
+                <p>${description}</p>
+              </body>
+              </html>      
+            `
+            response.writeHead(200);
+            response.end(template);
+          });
         });
       }
     } else {
