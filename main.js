b6ea391631e54587b8801154f9ab0b3e0ec56923
jhin93 <scene1993@gmail.com>
2021-11-12 11:29:18 +0900

    db에서 클릭한 id에 해당하는 칼럼 내용 가져오기

--- main.js
+++ main.js
@@ -34,30 +34,10 @@ var app = http.createServer(function(request,response){
     if(pathname === '/'){
       // 하지만 pathname만으로는 메인페이지와 사이드페이지들을 구분할 수 없기에 .id로 쿼리스트링이 undefined일 때 즉, 메인페이지를 조건으로 새롭게 렌더링.
       if(queryData.id === undefined){
-        /*  mysql로 대체.
-        // 파일 목록을 가져오는 코드로 감싸기. data 폴더의 내용물을 가져오면 콜백함수 실행.
-        fs.readdir('./data', function(error, filelist){
-          // 파일리스트 가져오고 렌더링파트(var template) 삽입.
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-
-          // 가져온 filelist 배열을 인자로 사용. 파일리스트 가져오기.
-          var list = template.list(filelist);
-          // 리팩토링 후엔 template 이란 변수를 쓰면 안됨. 위에 같은 이름으로 객체 만들었으니까.
-          var html = template.html(title, list, 
-            `<h2>${title}</h2><p>${description}</p>`,
-            // 홈페이지에서 쿼리스트링이 있는 경우에만, 즉 사이드페이지에서만 update가 될 수 있도록 pathname === '/' 조건에서는 `<a href="/update">update</a>`는 뺀다.
-            `<a href="/create">create</a>`
-            );
-          response.writeHead(200);
-          response.end(html);
-        })
-        */
         // mysql.js의 로직을 여기에 적용.
         // 두번째 인자에는 첫번째 인자인 sql문이 실행된 서버가 응답된 결과를 처리할 수 있도록 콜백함수를 줌.
         // 콜백함수의 대표형식은 첫번째 인자로 실패했을 때의 error, 두번째 인자로 성공했을 떄의 topics 
         db.query(`SELECT * FROM topic`, function(error, topics){
-          console.log(topics); // pm2 log로 확인. 확인한 결과물로 웹페이지 생성.
           var title = 'Welcome';
           var description = 'Hello, Node.js';
           // 가져온 filelist 배열을 인자로 사용. template.js 모듈에서 list 함수 가져와서 데이터(topics)대입.
@@ -73,6 +53,9 @@ var app = http.createServer(function(request,response){
           response.end(html);
         })
       } else {
+        // mysql 대체
+        // 글 리스트에서 글을 선택할 시 상세 보기.
+        /*
         // 여기부턴 쿼리스트링에 값이 있는 사이드 페이지들.
         fs.readdir('./data', function(error, filelist){
           // 보안을 위해 선언한 'path'에 parse 메소드를 쓰고 그 안에 보호대상을 삽입. 그리고 base까지만 읽도록 .base를 추가하면 그보다 상위 디렉토리는 읽을 수 없음. password.js 참고.
@@ -97,6 +80,44 @@ var app = http.createServer(function(request,response){
             response.end(html);
           });
         });
+        */
+        //  글 목록 먼저 가져오기
+        db.query(`SELECT * FROM topic`, function(error, topics){
+          // 에러발생 처리 (ex 데이터를 못받아왔을 때)
+          if(error){
+            throw error; // 여기서 nodejs가 이후의 코드진행을 중지하고 콘솔에 에러를 던짐.
+          }
+          // id 값에 따라 각 칼럼의 데이터를 가져오기. ex) id값이 3인 결과를 가져오는 sql문 - SELECT * FROM topic WHERE id = 3;
+          // 들어오는 id는 queryData.id. 처리하는 콜백함수에서 결과물은 topic에 담기.
+          // 익명의 누군가로부터 받은 입력값으로부터 db 보호를 위해 id=? 처리를 하고 두번째 인자로 배열에 데이터를 담는다.
+          db.query(`SELECT * FROM topic WHERE id=?`, [queryData.id] , function(error2, topic){
+            if(error2){
+              throw error; // 여기서 nodejs가 이후의 코드진행을 중지하고 콘솔에 에러를 던짐.
+            }
+            console.log(topic[0].title);
+            // topic은 배열에 클릭한 아이디(프라이머리 키)에 해당하는 1개의 객체가 배열에 담겨서 옴. 그래서 topic[0]으로 해야 내용물을 사용할 수 있음.
+            var title = topic[0].title; // 타이틀
+            var description = topic[0].description; // 본문
+            // 가져온 filelist 배열을 인자로 사용. template.js 모듈에서 list 함수 가져와서 데이터(topics)대입.
+            var list = template.list(topics);
+            // template.html에 각 인자들이 ,로 구분되어 대입되고, 그 결과물을 변수(html)에 담음. control엔 ui 요소담음(ex a 태그)
+            var html = template.html(title, list, 
+              `<h2>${title}</h2>${description}`,
+              // 홈페이지에서 쿼리스트링이 있는 경우에만, 즉 사이드페이지에서만 update가 될 수 있도록 pathname === '/' 조건에서는 `<a href="/update">update</a>`는 뺀다.
+              ` 
+                <a href="/create">create</a> 
+                <a href="/update?id=${queryData.id}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${queryData.id}">
+                  <input type="submit" value="delete">
+                </form>
+              `
+            );
+            // 웹페이지 마무리 대표형식
+            response.writeHead(200);
+            response.end(html);
+          })
+        })
       }
 
       // 글 작성(create) 로직.
